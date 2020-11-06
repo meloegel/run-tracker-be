2b859b53b3320e097581f2ad3af1f2697f943ea3
meloegel <meloegel@gmail.com>
2020-11-06 16:41:08 -0500

    added restricted-middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization
+    const secret = process.env.JWR_SECRET || 'asdouifhuiwwheuirhinasjkdfhbuiyghweuiygfbjhksdb'
+    if (token) {
+        jwt.verify(token, secret, (error, decodedToken) => {
+            if (error) {
+                console.log(error)
+                res.status(401).json({ error: ' Error with token' })
+            } else {
+                req.jwt = decodedToken
+                next()
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'You need a valid token' })
+    }
+}
