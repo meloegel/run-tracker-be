2988b905a830362a4cc07dea572a330b88eba176
meloegel <meloegel@gmail.com>
2020-11-07 23:35:00 -0500

    updated server file

---
+++ runTracker/runTracker-auth-router.js
@@ -0,0 +1,76 @@
+const express = require('express');
+
+const router = express.Router();
+
+const runTracker = require('./runTracker-model');
+
+// Gets a users runs //
+router.get('/runs/:id', (req, res) => {
+    const { id } = req.params
+    runTracker.getUserRuns(id)
+        .then(runs => {
+            if (runs) {
+                res.status(200).json(runs)
+            } else {
+                res.status(404).json({ Error: `Could not find runs for user with id: ${id}` })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ Error: `Error getting runs for user with id: ${id} ` })
+        })
+})
+
+// Posts a new run //
+router.post('/', (req, res) => {
+    const newRun = req.body
+    runTracker.addRun(newRun)
+        .then(run => {
+            res.status(201).json({ Posted: newRun })
+        })
+        .cacth(error => {
+            console.log(error)
+            res.status(500).json({ Error: 'Error adding run' })
+        })
+})
+
+// Updates a run //
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    runTracker.getAllRuns(id)
+        .then(run => {
+            if (run) {
+                runTracker.updateRun(changes, id)
+                    .then(updatedRun => {
+                        res.status(200).json({ Message: `Updated run with id: ${id}` })
+                    })
+            } else {
+                res.status(404).json({ Error: `Could not find run with id: ${id}` })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ Error: 'Failed to updated run' })
+        })
+})
+
+// Deletes a run //
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    runTracker.deleteRun(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(200).json({ Message: `Run with id: ${id} has been deleted` })
+            } else {
+                res.status(404).json({ Error: `Could not find run with id: ${id}` })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ Error: 'Error deleting run' })
+        })
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors')
+const cors = require('cors');
 
-
+const restrictedMiddleware = require('./auth/restricted-middleware');
 
 const server = express();
 
-
+const AuthRouter = require('./auth/auth-router');
+const UserRouter = require('./users/users-router');
+const runTrackerRouter = require('./runTracker/runTracker-router');
+const runTrackerAuthRouter = require('./runTracker/runTracker-auth-router');
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-
-
-
+server.use('/api');
+server.use('/api/auth', AuthRouter);
+server.use('/api/run-tracker', runTrackerRouter);
+server.use('api/auth/users', restrictedMiddleware, UserRouter);
+server.use('/api/auth/run-tracker', restrictedMiddleware, runTrackerAuthRouter);
 
 server.get('/', (req, res) => {
     const message = process.env.MESSAGE
