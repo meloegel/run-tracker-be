eb0c323738b0eec87f8ac14ca982d16ab13d5427
meloegel <meloegel@gmail.com>
2020-11-08 15:58:27 -0500

    added more seed data

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -2,7 +2,7 @@
 exports.seed = function (knex, Promise) {
   return knex('users').insert([
     { id: 1, username: 'Mark', password: 'asd', email: 'email@email.com' },
-    { id: 2, username: 'Steve', password: 'asd', email: 'testemail@email.com' },
-    { id: 3, username: 'Bob', password: 'asd', email: 'emailtest@email.com' }
+    { id: 2, username: 'RunnerDude', password: 'asd', email: 'testemail@email.com' },
+    { id: 3, username: 'JustKeepRunning', password: 'asd', email: 'emailtest@email.com' }
   ]);
 };
--- data/seeds/02-runTimes.js
+++ data/seeds/02-runTimes.js
@@ -9,6 +9,33 @@ exports.seed = function (knex, Promise) {
     },
     {
       id: 3, runTime: '33:08', distance: 4, publish: true, pace: '8:19', description: 'Easy pace', userId: 1
+    },
+    {
+      id: 4, runTime: '31:48', distance: 4, publish: true, pace: '7:53', description: 'Hard run but did not die, prefect weather!', userId: 1
+    },
+    {
+      id: 5, runTime: '37:08', distance: 4, publish: true, pace: '8:50', description: 'Great run!', userId: 2
+    },
+    {
+      id: 6, runTime: '42:08', distance: 6, publish: true, pace: '8:31', description: 'Great trail!', userId: 3
+    },
+    {
+      id: 7, runTime: '32:11', distance: 4, publish: true, pace: '8:09', description: 'Little knee pain but not bad', userId: 1
+    },
+    {
+      id: 8, runTime: '25:07', distance: 3, publish: true, pace: '8:18', description: 'Good run', userId: 1
+    },
+    {
+      id: 9, runTime: '19:08', distance: 2, publish: true, pace: '8:49', description: 'Too hot today', userId: 2
+    },
+    {
+      id: 10, runTime: '52:22', distance: 8, publish: true, pace: '8:01', description: 'Long run today', userId: 3
+    },
+    {
+      id: 11, runTime: '38:08', distance: 5, publish: true, pace: '7:59', description: 'Nice pace today', userId: 3
+    },
+    {
+      id: 12, runTime: '20:33', distance: 2, publish: true, pace: '9:19', description: 'Got out there today', userId: 2
     }
   ]);
 };
