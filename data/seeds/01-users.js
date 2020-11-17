1c79fcfcec6a48f2209e0616ff4601103e4c5799
meloegel <meloegel@gmail.com>
2020-11-16 20:33:42 -0500

    seed updates

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function (knex, Promise) {
   return knex('users').insert([
-    { id: 1, username: 'Mark', password: 'asdasd', location: 'Detroit, MI', email: 'email@email.com', avatar: 'https://cdn0.iconfinder.com/data/icons/world-issues/500/running_man-512.png' },
+    { id: 1, username: 'Mark', password: 'asdasd', location: 'Detroit, MI', email: 'email@email.com', avatar: 'https://img.etimg.com/thumb/msid-67536727,width-650,imgsize-779412,,resizemode-4,quality-100/running-shoes_gettyimages.jp' },
     { id: 2, username: 'RunnerDude', location: 'Devner, Co', password: 'asdasd', email: 'testemail@email.com', avatar: 'https://st4.depositphotos.com/3369547/25587/v/1600/depositphotos_255876378-stock-illustration-young-athletic-man-running-avatar.jpg' },
     { id: 3, username: 'JustKeepRunning', location: 'Miami, FL', password: 'asdasd', email: 'emailtest@email.com', avatar: 'https://images-na.ssl-images-amazon.com/images/I/5122iTN7t0L._AC_.jpg' }
   ]);
